3af069c4380812702a8a273dc778c43a8624d330
ZribiHaythem <zribihaythem@gmail.com>
2021-02-17 18:52:47 +0100

    init

--- Server/index.js
+++ Server/index.js
@@ -13,6 +13,7 @@ app.use(cookieParser());
 
 app.use("/api/blogs", router);
 app.use("/api/users", userRouters);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log("listening on port 3000!");
